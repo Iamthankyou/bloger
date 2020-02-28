345030b1fb6dfd49c2d275d29e6316fa93d65189
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-02-28 21:47:17 +0700

    Update Prog1.java

--- Prog1.java
+++ Prog1.java
@@ -2,8 +2,7 @@
 public class Program {
   public static void main(String args[]) {
     String webHost = “Host:https://google.com\n\r";
-    WebService service = new
-    WebService(webHost);
+    WebService service = new WebService(webHost);
     WebAdapter adapter = new WebAdapter();
     adapter.connect(service);
     WebClient client = new WebClient(adapter);
