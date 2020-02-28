4646911a7c6bc0d9c0133c708871d9d4532bda7c
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-02-28 21:18:36 +0700

    Create WebClient.java

---
+++ WebClient.java
@@ -0,0 +1,18 @@
+// duynotes blog
+public class WebClient {
+  private WebRequester webRequester;
+  public WebClient(WebRequester webRequester) {
+    this.webRequester = webRequester;
+  }
+  private Object makeObject() { … } // Make an Object
+  public void doWork() {
+    Object object = makeObject();
+    int status = webRequester.request(object);
+    if (status == 200) {
+      System.out.println("OK");
+    } else {
+      System.out.println("Not OK");
+    }
+    return;
+  }
+}
