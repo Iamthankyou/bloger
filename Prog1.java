84a03e5765934a1832dc01fbad5f57053dd5eae3
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-02-28 21:26:49 +0700

    Create Prog1.java

---
+++ Prog1.java
@@ -0,0 +1,12 @@
+// duynotes blog
+public class Program {
+  public static void main(String args[]) {
+    String webHost = “Host:https://google.com\n\r";
+    WebService service = new
+    WebService(webHost);
+    WebAdapter adapter = new WebAdapter();
+    adapter.connect(service);
+    WebClient client = new WebClient(adapter);
+    client.doWork();
+  }
+}
