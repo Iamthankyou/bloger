1d9e9620ffab89f62a2cbb9224077798c0355619
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-02-29 22:06:09 +0700

    Create Housing.java

---
+++ Housing.java
@@ -0,0 +1,29 @@
+// duynotes blog
+public class Housing implements IStructure {
+  private ArrayList<IStructure> structures;
+  private String address;
+  public Housing (String address) {
+    this.structures = new ArrayList<IStructure>();
+    this.address = address;
+  }
+  public String getName() {
+    return this.address;
+  }
+  public int addStructure(IStructure component) {
+    this.structures.add(component);
+    return this.structures.size() - 1;
+  }
+  public IStructure getStructure(int
+    componentNumber) {
+    return this.structures.get(componentNumber);
+  }
+  public void location() {
+    System.out.println("You are currently in " +
+    this.getName() + ". It has ");
+    for (IStructure struct : this.structures)
+      System.out.println(struct.getName());
+  }
+  /* Print out when you enter and exit the building */
+  public void enter() { … }
+  public void exit() { … }
+ }
