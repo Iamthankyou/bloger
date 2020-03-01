7a97c3314ac70b4d0992ab59a201fd58c1f7df4d
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-01 07:27:08 +0700

    Create Warehouse.java

---
+++ Warehouse.java
@@ -0,0 +1,18 @@
+// duynotes blog
+public class Warehouse implements IOrder {
+  private Hashtable<String, Integer> stock;
+  private String address;
+  /* Constructors and other attributes would go here */
+  //..
+  public void fulfillOrder(Order order) {
+    for (Item item : order.itemList)
+      this.stock.replace(item.sku,stock.get(item)-1);
+  /* Process the order for shipment and delivery */
+  //…
+  }
+  public int currentInventory(Item item) {
+    if (stock.containsKey(item.sku))
+      return stock.get(item.sku).intValue();
+    return 0;
+  }
+ }
