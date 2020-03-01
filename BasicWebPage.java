8be56cd5d35dda8b8a76d246cd980479510483e9
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-01 19:56:04 +0700

    Create BasicWebPage.java

---
+++ BasicWebPage.java
@@ -0,0 +1,9 @@
+// duynotes blog
+public class BasicWebPage implements WebPage {
+  public String html = ...
+  public String styleSheet = ...;
+  public String script = ...;
+  public void display() {
+  /* Renders the HTML to the stylesheet, and run any embedded scripts */
+  }
+ }
