fc8a867ac70ffca07a1f4ebc6803e0bd518bf7cb
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-02 22:28:57 +0700

    Create SpaghettiandPenne.java

---
+++ SpaghettiandPenne.java
@@ -0,0 +1,29 @@
+// duynotes blog
+public class SpaghettiMeatballs extends PastaDish {
+  public void addPasta() {
+    System.out.println("Add spaghetti");
+  }
+  public void addProtein() {
+    System.out.println("Add meatballs");
+  }
+  public void addSauce() {
+    System.out.println("Add tomato sauce");
+  }
+  public void addGarnish() {
+    System.out.println("Add Parmesan cheese");
+  }
+}
+
+public class PenneAlfredo extends PastaDish {
+  public void addPasta() {
+    System.out.println("Add penne");
+  }
+   public void addProtein() {
+    System.out.println("Add chicken");
+  }
+  public void addSauce() {
+    System.out.println("Add Alfredo sauce");
+  }
+  public void addGarnish() {
+    System.out.println("Add parsley");
+  }
