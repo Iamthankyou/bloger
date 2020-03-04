dd147b0bde6d7c291c752b6680950838c0d89427
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-04 14:48:02 +0700

    Create ClientSubsystem02.java

---
+++ ClientSubsystem02.java
@@ -0,0 +1,8 @@
+// duynotes blog
+public class ClientSubsystem {
+  public MergeSorting enterpriseSorting;
+  /* Constructors and other attributes go here */
+  public void sortInput(List customerList) {
+    this.enterpriseSorting.mergeSort(customerList);
+  }
+}
