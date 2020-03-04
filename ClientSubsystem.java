72b85eea0d732c7f9b15f0eeb0b3ae9704a1824b
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-04 14:42:54 +0700

    Create ClientSubsystem.java

---
+++ ClientSubsystem.java
@@ -0,0 +1,8 @@
+// duynotes blog
+public class ClientSubsystem {
+  public QuickSorting enterpriseSorting;
+  /* Constructors and other attributes go here */
+  public void sortInput(List customerList) {
+    this.enterpriseSorting.quickSort(customerList);
+  }
+}
