34ca723e8cff17dadc1ca77befd89774bd3aaae6
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-05 19:49:38 +0700

    Create Point3D.java

---
+++ Point3D.java
@@ -0,0 +1,24 @@
+// duynotes blog
+public class Point3D {
+  private int x;
+  private int y;
+  private int z;
+  public int getX() {
+    return x;
+  }
+  public int getY() {
+    return y;
+  }
+  public int getZ() {
+    return z;
+  }
+   public void setX(int newX) {
+    x = newX;
+  }
+  public void setY(int newY) {
+    y = newY;
+  }
+   public void setZ(int newZ) {
+  z = newZ;
+  }
+}
