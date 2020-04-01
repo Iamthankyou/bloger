dfc0fb437faf7b58bcfca630e56ee03b5c78dc58
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-01 22:24:20 +0700

    Create SingleNumber02.java

---
+++ SingleNumber02.java
@@ -0,0 +1,10 @@
+// duynotes.blogspot.com
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i:nums){
+            res= res^i;
+        }
+        return res;
+    }
+}
