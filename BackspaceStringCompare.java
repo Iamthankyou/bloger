66651bf547ad84027eeb78c6be26acce8fb1345b
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-09 17:20:52 +0700

    Create BackspaceStringCompare.java

---
+++ BackspaceStringCompare.java
@@ -0,0 +1,18 @@
+// duynotes.blogspot.com
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        // tks java with valueOf <3
+        return String.valueOf(ans);
+    }
+}
