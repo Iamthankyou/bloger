a1cd87ded1b1cad8025ea76881a169af9ddfe400
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-05-05 17:51:27 +0700

    Create FirstUniqueCharacterString.java

---
+++ FirstUniqueCharacterString.java
@@ -0,0 +1,17 @@
+//duynotes.blogspot.com
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (char i:s.toCharArray()){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        
+        for (int i=0; i<s.length(); i++){
+            if (map.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+} 
