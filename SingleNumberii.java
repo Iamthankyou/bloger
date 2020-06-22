008768946c2de93e4ac54fbe70ca526cf0945e3d
duy <quangduynb00@gmail.com>
2020-06-22 17:27:54 +0700

    Single Number II with bitwise

---
+++ SingleNumberii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int one = 0;
+        int two = 0;
+        
+        for (int i:nums){
+            one = (one^i) & (~two);
+            two = (two^i) & (~one);
+        }
+        
+        return one;
+    }
+}
