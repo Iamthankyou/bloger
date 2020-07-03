5bb07bd5377e94e52fb4203b95bcefb5daca46d2
iamthankyou <quangduynb00@gmail.com>
2020-07-03 23:59:06 +0700

    Pow with divide and conquer

---
+++ Pow.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n==0){
+            return 1;
+        }
+        
+        double half = myPow(x,n/2);
+        double res = half * half;
+        
+        if (n%2!=0){
+            res*=n<0?1.0/x:x;
+        }
+        
+        return res;
+    }
+    
+}
